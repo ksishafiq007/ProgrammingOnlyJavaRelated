e3ab833378010bf9d0ce6b844863b2afbd37459d
Shafiqul Islam <ksishafiq007@gmail.com>
2017-10-08 13:27:30 +0600

    TrianglePyramid

---
+++ src/programming/TrianglePyramid.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programming;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author KSI
+ */
+public class TrianglePyramid {
+    public static void main(String[] args) { 
+       
+        for(int m=0;m<5;m++){            
+            for(int n=1;n<=5-m;n++){                
+            System.out.print(n +" ");            
+        }
+       System.out.print('\n');
+      
+    }
+    }
+}
+    
+    
+
