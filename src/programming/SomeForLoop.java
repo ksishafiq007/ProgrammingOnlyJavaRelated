64d0e87e41cae2f61a9a8ee53bcd0725051cd3d7
Shafiqul Islam <ksishafiq007@gmail.com>
2017-10-18 14:45:37 +0600

    Some for loop

---
+++ src/programming/SomeForLoop.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programming;
+
+/**
+ *
+ * @author KSI
+ */
+public class SomeForLoop {
+    public static void main(String arg[]){
+        int n=10;
+        for(int i=0;i<n-1;i++){
+            System.out.print(+i +" ");
+                    
+        }
+        System.out.println();
+         for(int i=0;i<n;i++){
+            System.out.print(+i +" ");
+                    
+        }
+         System.out.println();
+          for(int i=0;i<=n;i++){
+            System.out.print(+i +" ");
+                    
+        }
+    }
+    
+}
