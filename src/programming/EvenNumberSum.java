2c8f3d1564506b18da80b78338e13e4f23da739b
Shafiqul Islam <ksishafiq007@gmail.com>
2017-12-16 22:06:11 +0600

    Even Number Summation

---
+++ src/programming/EvenNumberSum.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programming;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author KSI
+ */
+public class EvenNumberSum {
+    int sumofEvenNumber(int value){
+        int s=0,a,i;
+        a=value; 
+        //i=1 if Odd number will be count
+        for(i=2; i<=a;i=i+2){   
+            s=s+i;
+        }
+        return s;
+    }
+    public static void main(String[] args) { 
+        int sum;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Your Number:");
+        Integer value = scanner.nextInt();
+        sum=new EvenNumberSum().sumofEvenNumber(value);
+        System.out.println("Sum of Even Number: " +sum);
+    }
+    
+}
