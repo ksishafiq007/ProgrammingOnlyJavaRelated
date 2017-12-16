cf449eb753f8d23351c3e10cd235f578ca531f14
Shafiqul Islam <ksishafiq007@gmail.com>
2017-12-16 20:56:53 +0600

    Triangle Pyramid some edited and ok.

--- src/programming/TrianglePyramid.java
+++ src/programming/TrianglePyramid.java
@@ -13,13 +13,13 @@ import java.util.Scanner;
  */
 public class TrianglePyramid {
     public static void main(String[] args) { 
-       int k=5;
-        for(int m=0;m<5;m++){            
-            for(int n=1;n<=k;n++){                
+       
+        for(int m=0;m<=5;m++){            
+            for(int n=1;n<=m;n++){                
             System.out.print(n +" ");            
         }
        System.out.print('\n');
-      k--;
+      
     }
     }
 }
