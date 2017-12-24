56152524ab23f94e30cf336d40a465ea31365a4a
Shafiqul Islam <ksishafiq007@gmail.com>
2017-12-24 13:11:09 +0600

    Quiz check program

--- src/programming/Quiz.java
+++ src/programming/Quiz.java
@@ -8,25 +8,31 @@ package programming;
 public class Quiz {
 
     int x = 7;
+    
 
     public Quiz() {
+         System.out.println("kkkkkkkk"+x);
         x++;
-        System.out.println("*******a******" + x);
+        //System.out.println("*******a******" + x);
     }
 
     public Quiz(int i) {
+         System.out.println("p"+x);
+         System.out.println(i);
         x=x+i;
-        System.out.println("*******c****"+x);
+       System.out.println("*******c****"+x);
+       
     }
 
     public int getValue(int i) {
+        System.out.println("tttttt"+x);
         x += i;
-        System.out.println("*******b*******" + x);
+       System.out.println("*******b*******" + x);
         return x;
     }
 
     public static void main(String[] args) {
-     new Quiz(new Quiz().getValue(9));
+     new Quiz(new Quiz().getValue(3));
      
       
     }
