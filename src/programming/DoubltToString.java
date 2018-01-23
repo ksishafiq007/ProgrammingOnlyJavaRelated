e33b0d7234a0ba640ef3dcd94ee6844245f35852
Shafiqul Islam <ksishafiq007@gmail.com>
2018-01-23 13:50:22 +0600

    String to integer and double to string conversion.

---
+++ src/programming/DoubltToString.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programming;
+
+/**
+ *
+ * @author KSI
+ */
+public class DoubltToString {
+
+    public static void main(String args[]) {
+        double d = 89.7;
+        String s = Double.toString(d);
+        System.out.println(s);
+        String p="456";
+        int i=Integer.parseInt(p);
+        System.out.println(i);
+
+    }
+}
