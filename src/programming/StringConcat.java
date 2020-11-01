be0adde73690cac080f2e8d6bb6522ba700ba40b
ksishafiq007 <ksishafiq007@gmail.com>
2020-11-01 09:38:10 +0600

    string concat

---
+++ src/programming/StringConcat.java
@@ -0,0 +1,12 @@
+package programming;
+
+public class StringConcat {
+    public static void main(String[] args) {
+        String s1="1";
+        String s2=s1.concat("2");
+        System.out.println(s2);
+        s2.concat("3");
+        System.out.println(s2);
+
+    }
+}
