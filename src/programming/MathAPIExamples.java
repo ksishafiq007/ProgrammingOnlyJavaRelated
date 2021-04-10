252c8c20c96afaacd5bfb4fc7a47dac01c71ef0d
ksishafiq007 <ksishafiq007@gmail.com>
2021-04-10 15:29:45 +0600

    Absolute value with Math.abs()

---
+++ src/programming/MathAPIExamples.java
@@ -0,0 +1,16 @@
+package programming;
+
+public class MathAPIExamples {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = -19;
+		double x = 152.3;
+		double y = 0.349;
+		//absolute value
+		System.out.println("|" + a + "| is " + Math.abs(a));
+		System.out.println("|" + b + "| is " + Math.abs(b));
+		System.out.println("|" + x + "| is " + Math.abs(x));
+		System.out.println("|" + y + "| is " + Math.abs(y));
+	}
+
+}
