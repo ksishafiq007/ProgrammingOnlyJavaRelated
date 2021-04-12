49e233649fac876bf5d6a4a8754b31b232c380d2
ksishafiq007 <ksishafiq007@gmail.com>
2021-04-12 22:31:06 +0600

    rounding and ceiling add

--- src/programming/MathAPIExamples.java
+++ src/programming/MathAPIExamples.java
@@ -11,6 +11,25 @@ public class MathAPIExamples {
 		System.out.println("|" + b + "| is " + Math.abs(b));
 		System.out.println("|" + x + "| is " + Math.abs(x));
 		System.out.println("|" + y + "| is " + Math.abs(y));
+		
+		//sqrt(x)
+		int p=2;
+		int q=3;
+		System.out.println("The squire root of "+p+" is "+Math.sqrt(p));
+		System.out.println("The squire root of "+q+" is "+Math.sqrt(q));
+		
+		//Rounding functions
+		double m = 0.649;
+		System.out.println(x+" is approximately "+Math.round(x));
+		System.out.println(y+" is approximately "+Math.round(y));
+		System.out.println(m+" is approximately "+Math.round(m));
+		
+		//The "ceiling" of a number
+		System.out.println("The ceiling of "+a + " is "+Math.ceil(a));
+		System.out.println("The ceiling of "+b + " is "+Math.ceil(b));
+		System.out.println("The ceiling of "+x + " is "+Math.ceil(x));
+		System.out.println("The ceiling of "+y + " is "+Math.ceil(y));
+		
 	}
 
 }
