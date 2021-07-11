0aff99c73f5d4bf35166c5cde130a60f28565085
ksishafiq007 <ksishafiq007@gmail.com>
2021-07-12 00:16:57 +0600

    Factorial using Recursion example

--- src/programming/FactorialUsingRecursion.java
+++ src/programming/FactorialUsingRecursion.java
@@ -1,2 +1,18 @@
-package programming;public class FactorialUsingRecursion {
+package programming;
+
+public class FactorialUsingRecursion {
+    public static void main(String[] args) {
+        int i,fact=1;
+        int number=6;
+        fact=factorial(number);
+        System.out.println("Factorial of "+number+" is: "+fact);
+    }
+
+    static int factorial(int n){
+        if (n==0){
+            return 1;
+        } else {
+            return n*factorial(n-1);
+        }
+    }
 }
