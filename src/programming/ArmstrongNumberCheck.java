6dbf3a76ec144e23b7b70521a6711881102f20e1
ksishafiq007 <ksishafiq007@gmail.com>
2021-07-14 01:03:20 +0600

    Armstrong check

--- src/programming/ArmstrongNumberCheck.java
+++ src/programming/ArmstrongNumberCheck.java
@@ -2,9 +2,9 @@ package programming;
 
 import java.util.Scanner;
 
-public class ArmstsrongNumber {
-    static boolean isArmstrong(int m)
-    {
+public class ArmstrongNumberCheck {
+
+    static boolean isArmstrong(int m){
         int temp, digits=0, last=0, sum=0;
         temp = m;
         while(temp>0){
@@ -28,16 +28,16 @@ public class ArmstsrongNumber {
         }
     }
 
-    public static void main(String args[])
-    {
+    public static void main(String[] args) {
         int num;
         Scanner sc= new Scanner(System.in);
-        System.out.print("Enter the limit: ");
+        System.out.print("Enter the number: ");
         num=sc.nextInt();
-        System.out.println("Armstrong Number up to "+ num + " are: ");
-        for(int m=0; m<=num; m++)
-            if(isArmstrong(m)){
-                System.out.print(m+ ", ");
-            }
+
+        if (isArmstrong(num)){
+            System.out.println("Armstrong");
+        }else {
+            System.out.println("Not Armstrong");
+        }
     }
 }
