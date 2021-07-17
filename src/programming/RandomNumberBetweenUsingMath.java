0ab43d760f8a1da9808eff22b333af959cc12a7c
ksishafiq007 <ksishafiq007@gmail.com>
2021-07-17 20:40:44 +0600

    Random using Math class

---
+++ src/programming/RandomNumberBetweenUsingMath.java
@@ -0,0 +1,14 @@
+package programming;
+
+public class RandomNumberBetweenUsingMath {
+    public static void main(String[] args) {
+        int min = 300;
+        int max = 700;
+        System.out.println("Random value of type double between "+min+" to "+max+ ":");
+        double a = Math.random()*(max-min+1)+min;
+        System.out.println(a);
+        System.out.println("Random value of type int between "+min+" to "+max+ ":");
+        int b = (int)(Math.random()*(max-min+1)+min);
+        System.out.println(b);
+    }
+}
