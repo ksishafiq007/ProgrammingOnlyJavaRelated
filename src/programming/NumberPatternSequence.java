c40284db00ecda236814b2f135a918eeafb35501
ksishafiq007 <ksishafiq007@gmail.com>
2021-08-25 16:11:21 +0600

    number pattern sequence

---
+++ src/programming/NumberPatternSequence.java
@@ -0,0 +1,13 @@
+package programming;
+
+public class NumberPatternSequence {
+    public static void main(String[] args) {
+        int i,j,k=1;
+        for (i=1;i<=7;i++){
+            for (j=1;j<=i;j++){
+                System.out.print(k++ + " ");
+            }
+            System.out.println();
+        }
+    }
+}
