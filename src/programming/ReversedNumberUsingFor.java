0fdd3c82c449195bccf679eefa61fef315d2fdb4
ksishafiq007 <ksishafiq007@gmail.com>
2021-08-27 18:37:08 +0600

    Reverse number update using for loop

--- src/programming/ReversedNumberUsingFor.java
+++ src/programming/ReversedNumberUsingFor.java
@@ -8,5 +8,20 @@ public class ReversedNumberUsingFor {
             reverse = reverse * 10 + remainder;
         }
         System.out.println("The reverse of the given number is: " + reverse);
+
+        int number2 = 1234567, reverse2 = 0;
+        for (; number2 != 0;){
+            int remainder = number2 % 10;
+            reverse2 = reverse2 * 10 + remainder;
+            number2 = number2 / 10;
+        }
+        System.out.println("The reverse of the given number is: " + reverse2);
+
+        int number3 = 786, reverse3 = 0;
+        for (; number3 != 0; number3 = number3 / 10){
+            int remainder = number3 % 10;
+            reverse3 = reverse3 * 10 + remainder;
+        }
+        System.out.println("The reverse of the given number is: " + reverse3);
     }
 }
